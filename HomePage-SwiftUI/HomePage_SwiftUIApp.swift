9cac35f6af659fbdb30c61e32b1ad0f8f85abd47
kubracennet <kubracennetyavasoglu@gmail.com>
2023-04-09 15:46:02 +0300

    Initial Commit

---
+++ HomePage-SwiftUI/HomePage_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePage_SwiftUIApp.swift
+//  HomePage-SwiftUI
+//
+//  Created by Kübra Cennet Yavaşoğlu on 9.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct HomePage_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
